a955a0be67d807850e963da3dafcb85fbb323b28
afansi <arsene.fansi@gmail.com>
2019-01-23 15:39:01 -0500

    Update run_evaluation.sh

--- projects/omsignal/evaluation/run_evaluation.sh
+++ projects/omsignal/evaluation/run_evaluation.sh
@@ -7,6 +7,6 @@ source /rap/jvb-000-aa/COURS2019/etudiants/common.env
 PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/omsignal/'
 
 RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/projects/omsignal/evaluation'
-DATA_FILE='/rap/jvb-000-aa/COURS2019/etudiants/data/etudiants/data/omsignal/myHeartProject/sample_test.dat'
+DATA_FILE='/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/sample_test.dat'
 
 s_exec python $PROJECT_PATH/evaluation/eval.py --dataset=$DATA_FILE --results_dir=$RESULTS_DIR
