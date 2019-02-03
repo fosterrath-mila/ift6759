05cc87f152a0d1f820b1eae9d6148839a589d791
Mathieu Germain <mathieu.germain@gmail.com>
2019-02-03 17:36:38 -0500

    Update run_evaluation.sh

--- projects/horoma/evaluation/run_evaluation.sh
+++ projects/horoma/evaluation/run_evaluation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # PROJECT_PATH will be changed to the master branch of your repo. Make sure it contains `evaluation/eval.py`
-PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/'
+PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/horoma/'
 
-RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/evaluation'
-DATA_FILE='/rap/jvb-000-aa/COURS2019/etudiants/data/horoma/valid_x.dat'
+RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/horoma/evaluation'
+DATA_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/horoma/'
 
 cd $PROJECT_PATH/evaluation
 s_exec python eval.py --dataset_dir=$DATA_DIR --results_dir=$RESULTS_DIR
