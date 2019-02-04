bbff051877bcb10e21ec2488c995ffef64bdca59
Jeremy Pinto <jerpint@gmail.com>
2019-02-04 11:46:21 -0500

    Run python file from within evaluation/ folder

--- projects/humanware/evaluation/run_evaluation.sh
+++ projects/humanware/evaluation/run_evaluation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd "${PBS_O_WORKDIR}"
-source /rap/jvb-000-aa/COURS2019/etudiants/common.env
 
 # PROJECT_PATH will be changed to the master branch of your repo
 PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/'
@@ -10,4 +8,5 @@ RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/eval
 DATA_DIR='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/data/SVHN/test_sample'
 METADATA_FILENAME='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/data/SVHN/test_sample_metadata.pkl'
 
-s_exec python $PROJECT_PATH/evaluation/eval.py --dataset_dir=$DATA_DIR --results_dir=$RESULTS_DIR --metadata_filename=$METADATA_FILENAME
+cd $PROJECT_PATH/evaluation
+s_exec python eval.py --dataset_dir=$DATA_DIR --results_dir=$RESULTS_DIR --metadata_filename=$METADATA_FILENAME
