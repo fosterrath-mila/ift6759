fe503f5e49a881597f3b27afe15cfab8cb9b4e8c
Jeremy Pinto <jerpint@gmail.com>
2019-04-09 23:39:44 -0400

    Update paths, fix s_exec usage

--- projects/humanware/evaluation/run_evaluation.sh
+++ projects/humanware/evaluation/run_evaluation.sh
@@ -5,13 +5,13 @@
 export TEAM_NAME=b3phutN
 
 # PROJECT_PATH will designate the full path to the folder containing your code and repo, i.e.
-export PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/submissions/'$TEAM_NAME
+export PROJECT_PATH=/rap/jvb-000-aa/COURS2019/etudiants/submissions/$TEAM_NAME
 
 # RESULTS_DIR will contain the full path to Where the final evaluation results will be saved. In this case, the final evaluation result is $TEAM_NAME
-export RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/evaluation'
+export RESULTS_DIR=/rap/jvb-000-aa/COURS2019/etudiants/ift6759/projects/humanware/evaluation
 
 # Where the test data is located
-export DATA_DIR='/rap/jvb-000-aa/COURS2019/etudiants/Humwanware_v1_15535195/test'
+export DATA_DIR=/rap/jvb-000-aa/COURS2019/assistants/Humanware_v1_1553272293/test
 
 # Your project will be copied to the evaluator's home to ensure proper write permission, so you don't have to worry about those
 cp -r $PROJECT_PATH $HOME
@@ -20,4 +20,6 @@ cp -r $PROJECT_PATH $HOME
 # The code will be launched using s_exec, meaning that every subsequent script can be assumed to already be inside a singularity container.
 
 chmod +x $HOME/$TEAM_NAME/code/evaluation/bloc3_evaluation.sh # Make code executable
-s_exec $HOME/$TEAM_NAME/code/evaluation/bloc3_evaluation.sh
+
+cd $HOME/$TEAM_NAME
+s_exec code/evaluation/bloc3_evaluation.sh
