8e13907e0663a0671625881bef64d9aa71e9981b
Frédéric Osterrath <frederic.osterrath@mila.quebec>
2020-01-27 15:02:03 -0500

    Make group ACLs inheritable

--- facl_set.sh
+++ facl_set.sh
@@ -20,82 +20,82 @@ setfacl -m group:cq-training-1:r-x\
         project?/data
 
 # User group definitions
-setfacl -m user:guest111:rwx\
-        -m user:guest112:rwx\
-        -m user:guest113:rwx\
-        -m user:guest114:rwx\
+setfacl -d -m user:guest111:rwx\
+        -d -m user:guest112:rwx\
+        -d -m user:guest113:rwx\
+        -d -m user:guest114:rwx\
         project?/{submissions,teams}/team01
-setfacl -m user:guest115:---\
-        -m user:guest116:rwx\
-        -m user:guest117:---\
-        -m user:guest118:rwx\
-        -m user:guest162:rwx\
-        -m user:guest154:rwx\
+setfacl -d -m user:guest115:---\
+        -d -m user:guest116:rwx\
+        -d -m user:guest117:---\
+        -d -m user:guest118:rwx\
+        -d -m user:guest162:rwx\
+        -d -m user:guest154:rwx\
         project?/{submissions,teams}/team02
-setfacl -m user:guest119:rwx\
-        -m user:guest120:rwx\
-        -m user:guest121:rwx\
-        -m user:guest122:rwx\
+setfacl -d -m user:guest119:rwx\
+        -d -m user:guest120:rwx\
+        -d -m user:guest121:rwx\
+        -d -m user:guest122:rwx\
         project?/{submissions,teams}/team03
-setfacl -m user:guest123:rwx\
-        -m user:guest124:rwx\
-        -m user:guest125:rwx\
-        -m user:guest126:rwx\
+setfacl -d -m user:guest123:rwx\
+        -d -m user:guest124:rwx\
+        -d -m user:guest125:rwx\
+        -d -m user:guest126:rwx\
         project?/{submissions,teams}/team04
-setfacl -m user:guest127:rwx\
-        -m user:guest128:---\
-        -m user:guest129:rwx\
-        -m user:guest130:rwx\
-        -m user:guest170:rwx\
+setfacl -d -m user:guest127:rwx\
+        -d -m user:guest128:---\
+        -d -m user:guest129:rwx\
+        -d -m user:guest130:rwx\
+        -d -m user:guest170:rwx\
         project?/{submissions,teams}/team05
-setfacl -m user:guest131:rwx\
-        -m user:guest132:rwx\
-        -m user:guest133:rwx\
-        -m user:guest134:rwx\
+setfacl -d -m user:guest131:rwx\
+        -d -m user:guest132:rwx\
+        -d -m user:guest133:rwx\
+        -d -m user:guest134:rwx\
         project?/{submissions,teams}/team06
-setfacl -m user:guest135:rwx\
-        -m user:guest136:rwx\
-        -m user:guest137:rwx\
-        -m user:guest138:rwx\
+setfacl -d -m user:guest135:rwx\
+        -d -m user:guest136:rwx\
+        -d -m user:guest137:rwx\
+        -d -m user:guest138:rwx\
         project?/{submissions,teams}/team07
-setfacl -m user:guest139:rwx\
-        -m user:guest140:rwx\
-        -m user:guest141:rwx\
-        -m user:guest142:rwx\
+setfacl -d -m user:guest139:rwx\
+        -d -m user:guest140:rwx\
+        -d -m user:guest141:rwx\
+        -d -m user:guest142:rwx\
         project?/{submissions,teams}/team08
-setfacl -m user:guest143:rwx\
-        -m user:guest144:rwx\
-        -m user:guest145:rwx\
-        -m user:guest146:rwx\
+setfacl -d -m user:guest143:rwx\
+        -d -m user:guest144:rwx\
+        -d -m user:guest145:rwx\
+        -d -m user:guest146:rwx\
         project?/{submissions,teams}/team09
-setfacl -m user:guest147:rwx\
-        -m user:guest148:rwx\
-        -m user:guest149:rwx\
-        -m user:guest150:rwx\
+setfacl -d -m user:guest147:rwx\
+        -d -m user:guest148:rwx\
+        -d -m user:guest149:rwx\
+        -d -m user:guest150:rwx\
         project?/{submissions,teams}/team10
-setfacl -m user:guest151:rwx\
-        -m user:guest152:rwx\
-        -m user:guest153:rwx\
-        -m user:guest154:---\
-        -m user:guest169:rwx\
+setfacl -d -m user:guest151:rwx\
+        -d -m user:guest152:rwx\
+        -d -m user:guest153:rwx\
+        -d -m user:guest154:---\
+        -d -m user:guest169:rwx\
         project?/{submissions,teams}/team11
-setfacl -m user:guest155:rwx\
-        -m user:guest156:rwx\
-        -m user:guest157:rwx\
-        -m user:guest158:rwx\
+setfacl -d -m user:guest155:rwx\
+        -d -m user:guest156:rwx\
+        -d -m user:guest157:rwx\
+        -d -m user:guest158:rwx\
         project?/{submissions,teams}/team12
-setfacl -m user:guest159:rwx\
-        -m user:guest160:rwx\
-        -m user:guest161:rwx\
-        -m user:guest162:---\
-        -m user:guest168:rwx\
-        -m user:guest170:---\
+setfacl -d -m user:guest159:rwx\
+        -d -m user:guest160:rwx\
+        -d -m user:guest161:rwx\
+        -d -m user:guest162:---\
+        -d -m user:guest168:rwx\
+        -d -m user:guest170:---\
         project?/{submissions,teams}/team13
-setfacl -m user:guest163:rwx\
-        -m user:guest164:rwx\
-        -m user:guest165:rwx\
-        -m user:guest166:rwx\
-        -m user:guest167:---\
+setfacl -d -m user:guest163:rwx\
+        -d -m user:guest164:rwx\
+        -d -m user:guest165:rwx\
+        -d -m user:guest166:rwx\
+        -d -m user:guest167:---\
         project?/{submissions,teams}/team14
 
 popd
